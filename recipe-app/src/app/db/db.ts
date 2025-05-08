cd31b1ef2ccd02c31a48d102a5b992282974ff91
Glont Tudor <tudorglont05@gmail.com>
2025-05-08 16:22:24 +0300

    schimbare baza de date

--- recipe-app/src/app/db/db.ts
+++ recipe-app/src/app/db/db.ts
@@ -1,7 +1,7 @@
 import { i, init } from '@instantdb/core';
 
 
-const API_ID = '5b50c930-746e-4bd9-bdb0-05883a47619d';
+const API_ID = 'ec916851-6aef-4699-a878-1bb897fc02c0';
 const schema = i.schema({
   entities: {
     recipes: i.entity({
